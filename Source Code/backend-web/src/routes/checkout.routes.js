e376e6b42c03876a6f6870db051fc0bb4e4ce354
asssjdjd <phamthanhlong725@gmail.com>
2025-11-20 13:04:22 +0700

    remove old backend

--- Source Code/backend-web/src/routes/checkout.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import checkOutService from "../services/checkOut.services.js";
-
-const router = express.Router();
-
-router.post("/", checkOutService.checkOutParkingSession);
-
-export default router;
--- Source Code/backend-web/src/routes/viewReports.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import viewReportsController from "../services/viewReport.services.js";
-
-const router = express.Router();
-
-router.get("/", viewReportsController.viewReport);
-
-export default router;
